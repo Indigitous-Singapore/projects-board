49cd320aaad39dc8e5a961006cde4f40971c8d1d
Zi Jian <zj.lai21@gmail.com>
2020-08-28 01:51:09 +0800

    Added header

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -1,20 +1,40 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleDrawer(!leftDrawerOpen)"
-        />
-
-        <q-toolbar-title>
-          Projects Board
-        </q-toolbar-title>
-      </q-toolbar>
+  <q-layout view="hHh lpR fff">
+    <q-header bordered class="bg-white text-black">
+      <div class="row">
+        <q-toolbar class="col-grow">
+          <q-btn
+            flat
+            dense
+            round
+            icon="menu"
+            aria-label="Menu"
+            @click="toggleDrawer()"
+            class="lt-sm mobile-only"
+          />
+          <img src="../assets/placeholder.com-logo4.png" style="width:150px" class="q-mr-lg" />
+          <q-btn color="transparent" text-color="black" flat stretch padding="lg lg" label="ABOUT" to="/about" />
+          <q-btn color="transparent" text-color="black" flat stretch padding="lg lg" label="EXPLORE" to="/explore" />
+          <q-input 
+            v-model="search"
+            bg-color="transparent"
+            color="black"
+            clearable
+            borderless
+            hide-bottom-space
+            class="col-grow"
+          >
+            <template v-slot:prepend>
+              <q-icon name="search" />
+            </template>
+          </q-input>
+        </q-toolbar>
+        <q-separator vertical inset />
+        <q-toolbar class="col-auto q-mx-md">
+          <q-btn color="accent" bg-color="white" outline rounded padding="xs lg" label="START A PROJECT" class="accent" />
+          <q-btn color="transparent" text-color="black" flat stretch padding="lg lg" label="LOG IN" />
+        </q-toolbar>
+      </div>
     </q-header>
 
     <ComponentDrawer
@@ -24,28 +44,46 @@
     <q-page-container>
       <router-view />
     </q-page-container>
+
+    <q-footer bordered class="bg-grey-9 text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          Copyright 2020
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
   </q-layout>
 </template>
 
-<script>
+<script lang="ts">
 import ComponentDrawer from 'components/Structure/Drawer.vue'
-import { ref } from '@vue/composition-api'
+import { defineComponent, ref } from '@vue/composition-api'
  
-export default {
+export default defineComponent({
   name: 'MainLayout',
   components: {
     ComponentDrawer,
   },
-  setup(props, ctx) {
-    const leftDrawerOpen = ref(true)
-    const toggleDrawer = (open = true) => {
-      leftDrawerOpen.value  = open
+  setup () {
+    const leftDrawerOpen = ref(false)
+    const search = ref('')
+    const toggleDrawer = () => {
+      leftDrawerOpen.value  = !leftDrawerOpen.value
     }
 
     return {
       toggleDrawer,
       leftDrawerOpen,
+      search
     }
   }
-}
+})
 </script>
+<style lang="scss">
+.accent {
+  &:hover {
+    background-color: $accent !important;
+    color: white !important;
+  }
+}
+</style>
