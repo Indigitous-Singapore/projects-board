cf6550f188160ba4bc6abb48ff9b09026a424629
zjlai <zj.lai21@gmail.com>
2020-09-21 02:05:58 +0800

    Make header and menu drawer responsive

--- frontend/src/components/Structure/Drawer.vue
+++ frontend/src/components/Structure/Drawer.vue
@@ -1,11 +1,27 @@
 <template>
   <q-drawer
-    v-model="drawerOpen"
+    v-model="leftDrawerOpen"
     bordered
-    mini
-    content-class="bg-white"
+    class='full-height'
   >
     <q-list>
+      <q-item>
+        <q-item-section class="col justify-center">
+          <img src="../../assets/placeholder.com-logo4.png" style="width:150px" class="self-center" />
+        </q-item-section>
+      </q-item>
+      <q-item to="/project" exact>
+        <q-item-section>
+          <q-btn color="accent" bg-color="white" outline rounded label="START A PROJECT" class="accent" />
+        </q-item-section>
+      </q-item>
+      <!-- No Login for now
+      <q-item to="/login" exact>
+        <q-item-section>
+          <q-btn color="transparent" text-color="black" flat label="LOG IN" />
+        </q-item-section>
+      </q-item> --> 
+      <q-separator />
       <q-item to="/about" exact>
         <q-item-section>
           ABOUT
@@ -16,12 +32,30 @@
           EXPLORE
         </q-item-section>
       </q-item>
+      <q-item>
+        <q-item-section>
+          <q-input 
+            v-model="search"
+            clearable
+            hide-bottom-space
+            outlined
+            rounded
+            dense
+            placeholder="Search Projects"
+            class="col-grow"
+          >
+            <template v-slot:prepend>
+              <q-icon name="search" color="grey-9" />
+            </template>
+          </q-input>
+        </q-item-section>
+      </q-item>
     </q-list>
   </q-drawer>
 </template>
 
 <script>
-import { defineComponent } from '@vue/composition-api'
+import { defineComponent, computed, ref } from '@vue/composition-api'
 
 export default defineComponent({
   name: 'ComponentDrawer',
@@ -32,8 +66,10 @@ export default defineComponent({
     }
   },
   setup(props) {
+    const search = ref('')
     return {
-       drawerOpen: props.leftDrawerOpen
+      search,
+      drawerOpen: computed(() => props.leftDrawerOpen)
     }
   }
 })
