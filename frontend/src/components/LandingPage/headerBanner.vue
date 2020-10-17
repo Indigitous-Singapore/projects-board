3c11f0012ac7204141b5b79bb123a9e6b4c5ad66
aaron <aaron.lee@buooy.com>
2020-10-18 00:08:48 +0800

    Update headerBanner.vue

--- frontend/src/components/LandingPage/headerBanner.vue
+++ frontend/src/components/LandingPage/headerBanner.vue
@@ -6,13 +6,20 @@
         <p class="text-grey-9">
           Connecting Harvest to Labourers in the&nbsp;Digital&nbsp;Space
         </p>
+        <img
+          class="img-responsive q-pa-md sm-hide md-hide lg-hide xl-hide"
+          alt="Launchpad - collaborate"
+          src="/assets/collaborate.png"
+          />
         <div class="q-mt-xl justify-xs-between" style="max-width:420px">
           <q-btn color="accent" unelevated label="Explore" rounded size="1em" class="q-mr-md-lg q-mr-sm-sm" style="width:45%" />
           <q-btn color="grey-4" flat text-color="accent" label="LEARN MORE" rounded size="1em" style="width:45%" />
         </div>
       </div>
-      <div class="flex column col-sm-12 col-md-6 q-pa-md">
-        <q-img
+      <div class="xs-hide flex column col-sm-12 col-md-6 q-pa-md">
+        <img
+          class="img-responsive q-pa-md"
+          alt="Launchpad - collaborate"
           src="/assets/collaborate.png"
           />
       </div>
@@ -48,8 +55,23 @@ export default defineComponent({
     font-size: 1.65em;
   }
 }
+.img-responsive {
+  max-width: 100%
+}
 
-@media screen and (max-width: 991px) {
-  
+@media (max-width: 991px) {
+  #header-banner {
+    margin: 0 0 1em 0;
+  }
+  #header-text {
+    h1 {
+      line-height:1.25em;
+      font-size: 2.5em;
+      font-weight: 600;
+    }
+    p {
+      font-size: 1.25em;
+    }
+  }
 }
 </style>
